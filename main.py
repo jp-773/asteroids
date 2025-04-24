a696d5e62534293fd809a94f908968eb3c9b21ab
jp-773 <jparrett@gmail.com>
2025-04-23 19:04:04 -0500

    v3 triangle

--- main.py
+++ main.py
@@ -3,12 +3,15 @@
 # throughout this file
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     print("Starting Asteroids!")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
-    pygame.init
+    pygame.init()
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
@@ -17,8 +20,16 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        
         screen.fill("black")
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            player.rotation += 5  # Rotate counterclockwise
+        if keys[pygame.K_RIGHT]:
+            player.rotation -= 5  # Rotate clockwise
+
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
